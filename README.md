919f8a7cd5e65ee48dfc95ef9484202599a35adf
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-07-13 23:51:22 +0530

    added readme and boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+Solutions to the cses problem set
---
+++ boilerplate.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define vi vector<int>
+static auto _ = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+public:
+    double function(vector<int> &nums1, vector<int> &nums2)
+    {
+        
+    }
+};
+
+
+
+int main()
+{
+    vi nums1 {};
+    vi nums2 {2,3};
+    Solution s;
+    double ans = s.function(nums1, nums2);
+    cout << ans << endl;
+}
