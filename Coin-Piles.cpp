684865e25e99636bef7c05bd72f2b049c56ab907
Anirudh Kaushik <anirudh.ka@research.iiit.ac.in>
2023-07-14 19:49:10 +0530

    My solution to coin piles using linear programming

---
+++ Coin-Piles.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pb push_back
+#define mp make_pair
+#define pll pair<ll, ll>
+#define pii pair<int, int>
+#define vi vector<int>
+
+void solve()
+{
+    long long n, m;
+    cin >> n >> m;
+
+    // 2a + b = n
+    // 2b + a = m
+
+    if ((m + n) % 3 == 0 && m <= 2 * n && n <= 2 * m)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
